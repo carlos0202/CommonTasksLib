5a31426fe13ee269a3e806f975d8f78ff79e35c1
Carlos González <tnewlive@gmail.com>
2014-09-19 11:57:57 -0400

    Added Unit Tests

---
+++ LibTests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonTaskTests;
+
+namespace LibTests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void FormatWith_ValidFormat()
+        {
+            TestClass obja = new TestClass
+            {
+                FirstProperty = "Hi",
+                ReferenceProperty = DateTime.Now
+            };
+            TestClass objb = new TestClass
+            {
+                FirstProperty = "Second instance",
+                ReferenceProperty = DateTime.Now.AddDays(-1).AddSeconds(-100),
+                SelfReferencedProperty = obja
+            };
+            obja.SelfReferencedProperty = objb;
+            string format = "First : {FirstProperty} \n" +
+                              "Second : {ReferenceProperty}\n" +
+                              "Inner : {SelfReferencedProperty.SomeMethod()} \n";
+            string firstLine = "First : Hi ";
+            string formattedString = obja.FormatWith(format);
+            string actualLIne = formattedString.Substring(0, formattedString.IndexOf('\n'));
+            Console.WriteLine(obja.FormatWith(format));
+            Console.WriteLine();
+            Assert.AreEqual(firstLine, actualLIne, "Format With Failed");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FormatWith_NullFormat()
+        {
+            TestClass obja = new TestClass
+            {
+                FirstProperty = "Hi",
+                ReferenceProperty = DateTime.Now
+            };
+            TestClass objb = new TestClass
+            {
+                FirstProperty = "Second instance",
+                ReferenceProperty = DateTime.Now.AddDays(-1).AddSeconds(-100),
+                SelfReferencedProperty = obja
+            };
+            obja.SelfReferencedProperty = objb;
+            string format = "First : {FirstProperty} \n" +
+                              "Second : {ReferenceProperty}\n" +
+                              "Inner : {SelfReferencedProperty.SomeMethod()} \n";
+            string firstLine = "First : Hi ";
+            string formattedString = obja.FormatWith(null);
+            string actualLIne = formattedString.Substring(0, formattedString.IndexOf('\n'));
+            Console.WriteLine(obja.FormatWith(format));
+            Console.WriteLine();
+            Assert.AreEqual(firstLine, actualLIne, "Format With Failed");
+        }
+    }
+}
