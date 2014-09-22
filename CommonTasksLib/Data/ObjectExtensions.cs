c73215ae0e3de2a3c18c5334a66f9062ff24bf2e
Carlos González <tnewlive@gmail.com>
2014-09-22 09:27:11 -0400

    Added Transfer method
    Added support to transfer properties with the same name between class
    instances of any type, if the properties are assignable and convertible.

---
+++ CommonTasksLib/Data/ObjectExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTasksLib.Data
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Metodo utilitario privado para traspasar los valores
+        /// de las propiedades con igual nombre desde un objeto
+        /// a otro.
+        /// </summary>
+        /// <param name="source">Instancia del objeto del cual se obtendrán los datos.</param>
+        /// <param name="target">Instancia del objeto que recibirá los datos.</param>
+        static void Transfer(object source, object target, List<string> toSkip = null)
+        {
+            var sourceType = source.GetType(); //tipo de objeto de instancia fuente
+            var targetType = target.GetType(); //tipo de objeto de instancia destino
+
+            //creación de parámetros para la expresión lambda
+            var sourceParameter = Expression.Parameter(typeof(object), "source");
+            var targetParameter = Expression.Parameter(typeof(object), "target");
+
+            //creación de variables para la expresión lambda
+            var sourceVariable = Expression.Variable(sourceType, "castedSource");
+            var targetVariable = Expression.Variable(targetType, "castedTarget");
+
+            var expressions = new List<Expression>();
+            //agregar variables y parámetros a las expresiones lambda a ejecutar
+            expressions.Add(Expression.Assign(sourceVariable, Expression.Convert(sourceParameter, sourceType)));
+            expressions.Add(Expression.Assign(targetVariable, Expression.Convert(targetParameter, targetType)));
+
+            foreach (var property in sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                // verificar si la propiedad fuente admite lectura.
+                if (!property.CanRead) 
+                    continue;
+
+                // verificar si la propiedad no debe ser transferida.
+                if (toSkip != null)
+                    if (toSkip.Contains(property.Name, StringComparer.OrdinalIgnoreCase))
+                        continue;
+
+                var targetProperty = targetType.GetProperty(property.Name, BindingFlags.Public | BindingFlags.Instance);
+                if (targetProperty != null
+                        && targetProperty.CanWrite //se puede escribir en la propiedad de destino?
+                        && targetProperty.PropertyType.IsAssignableFrom(property.PropertyType))
+                {
+                    expressions.Add(
+                        Expression.Assign( //expresión para la asignación de las propiedades de los objetos.
+                            Expression.Property(targetVariable, targetProperty),
+                                Expression.Convert(
+                                    Expression.Property(sourceVariable, property), targetProperty.PropertyType)));
+                }
+            }
+
+            // creación formal de la expresión lambda a ejecutar.
+            var lambda =
+                Expression.Lambda<Action<object, object>>(
+                    Expression.Block(new[] { sourceVariable, targetVariable }, expressions),
+                    new[] { sourceParameter, targetParameter });
+
+            var del = lambda.Compile(); //compilar expresión lambda y obtener el delegado.
+
+            del(source, target); //ejectuar la expresión lambda utilizando el delegado obtenido.
+        }
+
+        /// <summary>
+        /// Metodo para copiar los datos de propiedades con igual nombre
+        /// desde una instancia de una clase hacia otra.
+        /// </summary>
+        /// <typeparam name="SourceType">Tipo de datos del objeto fuente (proveedor de datos)</typeparam>
+        /// <typeparam name="TargetType">Tipo de datos del objeto destino (receptor de datos)</typeparam>
+        /// <param name="source">Instancia del objeto fuente de los datos.</param>
+        /// <param name="targetObj">Instancia opcional del objeto recibidor de los datos</param>
+        /// <returns></returns>
+        public static void Transfer<SourceType, TargetType>(this SourceType source, ref TargetType targetObj, string toSkip = null)
+            where TargetType : class, new()
+             where SourceType: class
+        {
+            if (targetObj == null)
+            {
+                targetObj = new TargetType();
+            }
+            if (toSkip != null)
+            {
+                List<string> skipList = toSkip.Split(',').Where(s => !String.IsNullOrEmpty(s))
+                    .Select(s => s.Trim()).ToList();
+                Transfer(source, targetObj, skipList);
+            }
+            else
+            {
+                Transfer(source, targetObj);
+            }
+        }
+    }
+}
---
+++ LibTests/ObjectExtensionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonTasksLib.Data;
+
+namespace LibTests
+{
+    [TestClass]
+    public class ObjectExtensionTests
+    {
+        [TestMethod]
+        public void Transfer_NullDestination()
+        {
+            TestClass obja = new TestClass
+            {
+                FirstProperty = "Hi",
+                ReferenceProperty = DateTime.Now
+            };
+            TestClass objb = null;
+
+            obja.Transfer(ref objb);
+
+            Assert.AreEqual(obja.FirstProperty, objb.FirstProperty, "Transfer operation failed.");
+        }
+
+        [TestMethod]
+        public void Transfer_USingSkip()
+        {
+            TestClass obja = new TestClass
+            {
+                FirstProperty = "Hi",
+                ReferenceProperty = DateTime.Now
+            };
+            TestClass objb = null;
+
+            obja.Transfer(ref objb, "FirstProperty, ReferenceProperty");
+
+            Assert.AreEqual(null, objb.FirstProperty, "Transfer operation failed.");
+        }
+    }
+}
