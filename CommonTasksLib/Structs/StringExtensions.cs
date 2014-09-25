b1bb542b852764db2b2b875c6022d62654c47a83
Carlos González <tnewlive@gmail.com>
2014-09-25 17:00:33 -0400

    Sync

--- CommonTasksLib/Structs/StringExtensions.cs
+++ CommonTasksLib/Structs/StringExtensions.cs
@@ -11,17 +11,43 @@ namespace CommonTasksLib.Structs
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Método extensión para mejorar el string.format, con la habilidad
+        /// de aceptar los nombres de las propiedades de una clase, o de un 
+        /// objeto anónimo como placeholders de reemplazo.
+        /// </summary>
+        /// <param name="format">Formato a utilizar.</param>
+        /// <param name="source">Objeto utilizado para formatear.</param>
+        /// <returns>Un objeto string formateado con el formato especificado.</returns>
         public static string FormatWith(this string format, object source)
         {
             return FormatWith(format, null, source);
         }
 
+        /// <summary>
+        /// Método extensión para mejorar el string.format, con la habilidad
+        /// de aceptar los nombres de las propiedades de una clase, o de un 
+        /// objeto anónimo como placeholders de reemplazo.
+        /// </summary>
+        /// <typeparam name="T">Tipo de datos del objeto usado para formatear.</typeparam>
+        /// <param name="format">Formato a utilizar.</param>
+        /// <param name="source">Objeto utilizado para formatear.</param>
+        /// <returns>Un objeto string formateado con el formato especificado.</returns>
         public static string FormatWith<T>(this T source, string format)
             where T : class
         {
             return source.FormatWith(format, null);
         }
 
+        /// <summary>
+        /// Método extensión para mejorar el string.format, con la habilidad
+        /// de aceptar los nombres de las propiedades de una clase, o de un 
+        /// objeto anónimo como placeholders de reemplazo.
+        /// </summary>
+        /// <param name="format">Formato a utilizar.</param>
+        /// <param name="provider">Objeto para controlar el proceso de formato.</param>
+        /// <param name="source">Objeto utilizado para formatear.</param>
+        /// <returns>Un objeto string formateado con el formato especificado.</returns>
         public static string FormatWith(this string format, IFormatProvider provider, object source)
         {
             if (format == null)
@@ -49,6 +75,16 @@ namespace CommonTasksLib.Structs
             return string.Format(provider, rewrittenFormat, values.ToArray());
         }
 
+        /// <summary>
+        /// Método extensión para mejorar el string.format, con la habilidad
+        /// de aceptar los nombres de las propiedades de una clase, o de un 
+        /// objeto anónimo como placeholders de reemplazo.
+        /// </summary>
+        /// <typeparam name="T">Tipo de datos del objeto usado para formatear.</typeparam>
+        /// <param name="source">Objeto utilizado para formatear.</param>
+        /// <param name="format">Formato a utilizar.</param>
+        /// <param name="provider">Objeto para controlar el proceso de formato.</param>
+        /// <returns>Un objeto string formateado con el formato especificado.</returns>
         public static string FormatWith<T>(this T source, string format, IFormatProvider provider)
             where T : class
         {
@@ -95,6 +131,13 @@ namespace CommonTasksLib.Structs
             return string.Format(provider, rewrittenFormat, values.ToArray());
         }
 
+        /// <summary>
+        /// Método extensión utilizado para obtener una cantidad específica de 
+        /// caracteres de una cadena desde el inicio de la misma.
+        /// </summary>
+        /// <param name="source">Cadena de la cual obtener los caracteres.</param>
+        /// <param name="length">Cantidad de caracteres a extraer.</param>
+        /// <returns>Un objeto string con la cantidad de caracteres obtenidos.</returns>
         public static string Left(this string source, int length)
         {
             source = (source ?? string.Empty);
@@ -103,6 +146,14 @@ namespace CommonTasksLib.Structs
             return source.Substring(0, Math.Min(length, source.Length));
         }
 
+        /// <summary>
+        /// Método extensión utilizado para obtener una cantidad específica de 
+        /// caracteres de una cadena desde el final de la misma contando una cantidad
+        /// específica.
+        /// </summary>
+        /// <param name="source">Cadena de la cual obtener los caracteres.</param>
+        /// <param name="length">Cantidad de caracteres a extraer.</param>
+        /// <returns>Un objeto string con la cantidad de caracteres obtenidos.</returns>
         public static string Right(this string source, int length)
         {
             source = (source ?? string.Empty);
@@ -112,6 +163,11 @@ namespace CommonTasksLib.Structs
                 source.Substring(source.Length - length, length) : source;
         }
 
+        /// <summary>
+        /// Método extensión utilizado para generar una contraseña aleatoria.
+        /// </summary>
+        /// <param name="length">Longitud de la contraseña a generar.</param>
+        /// <returns>Un objeto String con la contraseña generada.</returns>
         public static string CreateRandomPassword(int length)
         {
             string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#_";
@@ -126,6 +182,12 @@ namespace CommonTasksLib.Structs
             return randomPassword;
         }
 
+        /// <summary>
+        /// Método extensión utilizado para generar una contraseña aleatoria.
+        /// </summary>
+        /// <param name="length">Longitud de la contraseña a generar.</param>
+        /// <param name="validChars">Conjunto de caracteres validos usados para generar la contraseña.</param>
+        /// <returns>Un objeto String con la contraseña generada.</returns>
         public static string CreateRandomPassword(int length, string validChars)
         {
             string randomPassword = "";
