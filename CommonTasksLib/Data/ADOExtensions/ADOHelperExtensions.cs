936fa9743cd3abf327163c7f580add2e5d061959
Carlos González <tnewlive@gmail.com>
2015-01-16 16:54:57 -0400

    Sync
    ToExpandoArray Updated

--- CommonTasksLib/Data/ADOExtensions/ADOHelperExtensions.cs
+++ CommonTasksLib/Data/ADOExtensions/ADOHelperExtensions.cs
@@ -10,16 +10,31 @@ namespace CommonTasksLib.Data.ADOExtensions
 {
     public static class ADOHelperExtensions
     {
-        public static List<ExpandoObject> ToExpandoArray<T>(this T source) where T: DbDataReader
+        public static List<ExpandoObject> ToExpandoArray<T>(this T source, string includedFields = null, string excludedFields = null) 
+            where T: DbDataReader
         {
             List<ExpandoObject> result = new List<ExpandoObject>();
+            List<string> ToInclude = includedFields == null ?
+                new List<string>() : includedFields.Split(new string[]{","}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            ToInclude.ForEach(i => i = i.Trim());
+            List<string> ToExclude = excludedFields == null ?
+                new List<String>() : excludedFields.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            ToExclude.ForEach(i => i = i.Trim());
             source.Select(r => r).AsParallel().ForAll(row =>
             {
                 ExpandoObject tmpRow = new ExpandoObject();
                 var DataColumn = (ICollection<KeyValuePair<string, object>>)tmpRow;
                 for (int i = 0; i < row.FieldCount; i++)
                 {
-                    DataColumn.Add(new KeyValuePair<string, object>(row.GetName(i), row.GetValue(i)));
+                    if (ToExclude.Contains(row.GetName(i), StringComparer.InvariantCultureIgnoreCase) && 
+                        !ToInclude.Contains(row.GetName(i), StringComparer.InvariantCultureIgnoreCase))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        DataColumn.Add(new KeyValuePair<string, object>(row.GetName(i), row.GetValue(i)));
+                    }
                 }
                 result.Add(tmpRow);
             });
@@ -29,3 +44,4 @@ namespace CommonTasksLib.Data.ADOExtensions
 
     }
 }
+
