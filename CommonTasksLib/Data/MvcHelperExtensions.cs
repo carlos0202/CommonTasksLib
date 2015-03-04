6224a020849a36cb95ad2b1ae5f97622c1baee0f
Carlos González <tnewlive@gmail.com>
2015-03-04 13:31:29 -0400

    Actionlinks

--- CommonTasksLib/Data/MvcHelperExtensions.cs
+++ CommonTasksLib/Data/MvcHelperExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -604,5 +605,101 @@ namespace CommonTasksLib.Data
                 }
             }
         }
+
+        //Authorization Links, Default implementations
+        public static bool ActionAuthorized(this HtmlHelper htmlHelper, string actionName, string controllerName)
+        {
+            ControllerBase controllerBase = string.IsNullOrEmpty(controllerName) ?
+                htmlHelper.ViewContext.Controller : htmlHelper.GetControllerByName(controllerName);
+            ControllerContext controllerContext = new ControllerContext(htmlHelper.ViewContext.RequestContext, controllerBase);
+            ControllerDescriptor controllerDescriptor = new ReflectedControllerDescriptor(controllerContext.Controller.GetType());
+            ActionDescriptor actionDescriptor = controllerDescriptor.FindAction(controllerContext, actionName);
+
+            if (actionDescriptor == null)
+                return false;
+
+            FilterInfo filters = new FilterInfo(FilterProviders.Providers.GetFilters(controllerContext, actionDescriptor));
+
+            AuthorizationContext authorizationContext = new AuthorizationContext(controllerContext, actionDescriptor);
+            foreach (IAuthorizationFilter authorizationFilter in filters.AuthorizationFilters)
+            {
+                authorizationFilter.OnAuthorization(authorizationContext);
+                if (authorizationContext.Result != null)
+                    return false;
+            }
+            return true;
+        }
+
+        public static ControllerBase GetControllerByName(this HtmlHelper htmlHelper, string controllerName)
+        {
+            IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+            IController controller = factory.CreateController(htmlHelper.ViewContext.RequestContext, controllerName);
+            if (controller == null)
+            {
+                throw new InvalidOperationException(
+                    String.Format(
+                        CultureInfo.CurrentCulture,
+                        "The IControllerFactory '{0}' did not return a controller for the name '{1}'. Controller not Found.",
+                        factory.GetType(), controllerName));
+            }
+            return (ControllerBase)controller;
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, null, new RouteValueDictionary(), new RouteValueDictionary(), showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, object routeValues, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, null, new RouteValueDictionary(routeValues), new RouteValueDictionary(), showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, controllerName, new RouteValueDictionary(), new RouteValueDictionary(), showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, RouteValueDictionary routeValues, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, null, routeValues, new RouteValueDictionary(), showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, object routeValues, object htmlAttributes, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, null, new RouteValueDictionary(routeValues), new RouteValueDictionary(htmlAttributes), showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, null, routeValues, htmlAttributes, showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, object routeValues, object htmlAttributes, bool showActionLinkAsDisabled = false)
+        {
+            return htmlHelper.ActionLinkAuth(linkText, actionName, controllerName, new RouteValueDictionary(routeValues), new RouteValueDictionary(htmlAttributes), showActionLinkAsDisabled);
+        }
+
+        public static MvcHtmlString ActionLinkAuth(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes, bool showActionLinkAsDisabled)
+        {
+            if (htmlHelper.ActionAuthorized(actionName, controllerName))
+            {
+                return htmlHelper.ActionLink(linkText, actionName, controllerName, routeValues, htmlAttributes);
+            }
+            else
+            {
+                if (showActionLinkAsDisabled)
+                {
+                    TagBuilder tagBuilder = new TagBuilder("span");
+                    tagBuilder.InnerHtml = linkText;
+                    return MvcHtmlString.Create(tagBuilder.ToString());
+                }
+                else
+                {
+                    return MvcHtmlString.Empty;
+                }
+            }
+        }
+
     }
 }
